81b3b8e7ba3dbed277462d29cf0c15a6e220aba4
Márcio César <marcinho060498@gmail.com>
2024-01-27 23:56:17 -0300

    Criando novos produtos

--- script.js
+++ script.js
@@ -50,10 +50,135 @@ const fruta = {
     qtd: 0
 }
 
-
 const trufa = {
     descricao: 'TRUFA GELADA',
     valor: 4.80,
+    qtd: 0
+}
+
+const retro = {
+    descricao: 'RETRO',
+    valor: 1.50,
+    qtd: 0
+}
+
+const astros = {
+    descricao: 'ASTROS',
+    valor: 4.50,
+    qtd: 0
+}
+
+const natturale = {
+    descricao: 'NATTURALE',
+    valor: 3.90,
+    qtd: 0
+}
+
+const acai = {
+    descricao: 'AÇAI',
+    valor: 4.75,
+    qtd: 0
+}
+
+const snack = {
+    descricao: 'SNACK',
+    valor: 5.90,
+    qtd: 0
+}
+
+const fondue = {
+    descricao: 'FONDUE',
+    valor: 3.80,
+    qtd: 0
+}
+
+const love = {
+    descricao: 'LOVE',
+    valor: 3.00,
+    qtd: 0
+}
+
+const unicornio = {
+    descricao: 'UNICORNIO',
+    valor: 3.90,
+    qtd: 0
+}
+
+const limaoSiciliano = {
+    descricao: 'LIMÃO SICILIANO',
+    valor: 4.90,
+    qtd: 0
+}
+
+const alpha = {
+    descricao: 'ALPHA PREMIUM',
+    valor: 5.90,
+    qtd: 0
+}
+
+const belgaBrulee = {
+    descricao: 'BELGA / BRULEE',
+    valor: 7.50,
+    qtd: 0
+}
+
+const paletas = {
+    descricao: 'PALETAS',
+    valor: 6.90,
+    qtd: 0
+}
+
+const brownieCenoura = {
+    descricao: 'BROWNIE / CENOURA',
+    valor: 8.50,
+    qtd: 0
+}
+
+const poteCopao = {
+    descricao: 'COPÃO',
+    valor: 5.75,
+    qtd: 0
+}
+
+const poteSundae = {
+    descricao: 'SUNDAE',
+    valor: 5.90,
+    qtd: 0
+}
+
+const acaiGranola = {
+    descricao: 'AÇAI C/ GRANOLA',
+    valor: 8.90,
+    qtd: 0
+}
+
+const poteDuo1eMeio = {
+    descricao: 'POTE DUO 1,5L',
+    valor: 15.90,
+    qtd: 0
+}
+
+const pote2l = {
+    descricao: 'POTE 2L',
+    valor: 19.90,
+    qtd: 0
+}
+
+const specialle = {
+    descricao: 'POTE SPECIALLE',
+    valor: 26.90,
+    qtd: 0
+}
+
+const poteAcai2l = {
+    descricao: 'POTE AÇAI 2L',
+    valor: 37.90,
+    qtd: 0
+}
+
+const acaiZero = {
+    descricao: 'POTE AÇAI ZERO',
+    valor: 37.90,
     qtd: 0
 }
 
@@ -90,7 +215,7 @@ function picoleAoLeite(){
 
 
 // Repetimos o mesmo processo para todos os outros produtos
-function picoleAoLeiteFruta(){
+function picoleFruta(){
     fruta.qtd = Number(quantidade.value);
     if(fruta.qtd === 0){
         fruta.qtd = 1;
@@ -111,6 +236,279 @@ function picoleTrufaGelada(){
     const copiaTrufaGelada = Object.assign({}, trufa);
     lista.push(copiaTrufaGelada);
     let mult = copiaTrufaGelada.qtd * copiaTrufaGelada.valor
+    precoAtualizado.push(Number(mult))
+    mostrarLista()
+}
+
+
+function picoleRetro(){
+    retro.qtd = Number(quantidade.value);
+    if(retro.qtd === 0){
+        retro.qtd = 1;
+    }
+    const copiaRetro = Object.assign({}, retro);
+    lista.push(copiaRetro);
+    let mult = copiaRetro.qtd * copiaRetro.valor
+    precoAtualizado.push(Number(mult))
+    mostrarLista()
+}
+
+
+function picoleAstros(){
+    astros.qtd = Number(quantidade.value);
+    if(astros.qtd === 0){
+        astros.qtd = 1;
+    }
+    const copiaAstros = Object.assign({}, astros);
+    lista.push(copiaAstros);
+    let mult = copiaAstros.qtd * copiaAstros.valor
+    precoAtualizado.push(Number(mult))
+    mostrarLista()
+}
+
+
+function picoleNatturale(){
+    natturale.qtd = Number(quantidade.value);
+    if(natturale.qtd === 0){
+        natturale.qtd = 1;
+    }
+    const copiaNatturale = Object.assign({}, natturale);
+    lista.push(copiaNatturale);
+    let mult = copiaNatturale.qtd * copiaNatturale.valor
+    precoAtualizado.push(Number(mult))
+    mostrarLista()
+}
+
+
+function picoleAcai(){
+    acai.qtd = Number(quantidade.value);
+    if(acai.qtd === 0){
+        acai.qtd = 1;
+    }
+    const copiaAcai = Object.assign({}, acai);
+    lista.push(copiaAcai);
+    let mult = copiaAcai.qtd * copiaAcai.valor
+    precoAtualizado.push(Number(mult))
+    mostrarLista()
+}
+
+
+function picoleSnack(){
+    snack.qtd = Number(quantidade.value);
+    if(snack.qtd === 0){
+        snack.qtd = 1;
+    }
+    const copiaSnack = Object.assign({}, snack);
+    lista.push(copiaSnack);
+    let mult = copiaSnack.qtd * copiaSnack.valor
+    precoAtualizado.push(Number(mult))
+    mostrarLista()
+}
+
+
+function picoleFondue(){
+    fondue.qtd = Number(quantidade.value);
+    if(fondue.qtd === 0){
+        fondue.qtd = 1;
+    }
+    const copiaFondue = Object.assign({}, fondue);
+    lista.push(copiaFondue);
+    let mult = copiaFondue.qtd * copiaFondue.valor
+    precoAtualizado.push(Number(mult))
+    mostrarLista()
+}
+
+
+function picoleLove(){
+    love.qtd = Number(quantidade.value);
+    if(love.qtd === 0){
+        love.qtd = 1;
+    }
+    const copiaLove = Object.assign({}, love);
+    lista.push(copiaLove);
+    let mult = copiaLove.qtd * copiaLove.valor
+    precoAtualizado.push(Number(mult))
+    mostrarLista()
+}
+
+
+function picoleUnicornio(){
+    unicornio.qtd = Number(quantidade.value);
+    if(unicornio.qtd === 0){
+        unicornio.qtd = 1;
+    }
+    const copiaUnicornio = Object.assign({}, unicornio);
+    lista.push(copiaUnicornio);
+    let mult = copiaUnicornio.qtd * copiaUnicornio.valor
+    precoAtualizado.push(Number(mult))
+    mostrarLista()
+}
+
+
+function picoleLimaoSiciliano(){
+    limaoSiciliano.qtd = Number(quantidade.value);
+    if(limaoSiciliano.qtd === 0){
+        limaoSiciliano.qtd = 1;
+    }
+    const copiaLimaoSiciliano = Object.assign({}, limaoSiciliano);
+    lista.push(copiaLimaoSiciliano);
+    let mult = copiaLimaoSiciliano.qtd * copiaLimaoSiciliano.valor
+    precoAtualizado.push(Number(mult))
+    mostrarLista()
+}
+
+
+function picoleAlpha(){
+    alpha.qtd = Number(quantidade.value);
+    if(alpha.qtd === 0){
+        alpha.qtd = 1;
+    }
+    const copiaAlpha = Object.assign({}, alpha);
+    lista.push(copiaAlpha);
+    let mult = copiaAlpha.qtd * copiaAlpha.valor
+    precoAtualizado.push(Number(mult))
+    mostrarLista()
+}
+
+
+function picoleBelgaBrulee(){
+    belgaBrulee.qtd = Number(quantidade.value);
+    if(belgaBrulee.qtd === 0){
+        belgaBrulee.qtd = 1;
+    }
+    const copiaBelgaBrulee = Object.assign({}, belgaBrulee);
+    lista.push(copiaBelgaBrulee);
+    let mult = copiaBelgaBrulee.qtd * copiaBelgaBrulee.valor
+    precoAtualizado.push(Number(mult))
+    mostrarLista()
+}
+
+
+function picolePaleta(){
+    paletas.qtd = Number(quantidade.value);
+    if(paletas.qtd === 0){
+        paletas.qtd = 1;
+    }
+    const copiaPaletas = Object.assign({}, paletas);
+    lista.push(copiaPaletas);
+    let mult = copiaPaletas.qtd * copiaPaletas.valor
+    precoAtualizado.push(Number(mult))
+    mostrarLista()
+}
+
+
+function picoleBrownieCenoura(){
+    brownieCenoura.qtd = Number(quantidade.value);
+    if(brownieCenoura.qtd === 0){
+        brownieCenoura.qtd = 1;
+    }
+    const copiaBrownieCenoura = Object.assign({}, brownieCenoura);
+    lista.push(copiaBrownieCenoura);
+    let mult = copiaBrownieCenoura.qtd * copiaBrownieCenoura.valor
+    precoAtualizado.push(Number(mult))
+    mostrarLista()
+}
+
+
+function potinhoCopao(){
+    poteCopao.qtd = Number(quantidade.value);
+    if(poteCopao.qtd === 0){
+        poteCopao.qtd = 1;
+    }
+    const copiaPoteCopao = Object.assign({}, poteCopao);
+    lista.push(copiaPoteCopao);
+    let mult = copiaPoteCopao.qtd * copiaPoteCopao.valor
+    precoAtualizado.push(Number(mult))
+    mostrarLista()
+}
+
+
+function potinhoSundae(){
+    poteSundae.qtd = Number(quantidade.value);
+    if(poteSundae.qtd === 0){
+        poteSundae.qtd = 1;
+    }
+    const copiaPoteSundae = Object.assign({}, poteSundae);
+    lista.push(copiaPoteSundae);
+    let mult = copiaPoteSundae.qtd * copiaPoteSundae.valor
+    precoAtualizado.push(Number(mult))
+    mostrarLista()
+}
+
+
+function potinhoAcai(){
+    acaiGranola.qtd = Number(quantidade.value);
+    if(acaiGranola.qtd === 0){
+        acaiGranola.qtd = 1;
+    }
+    const copiaAcaiGranola = Object.assign({}, acaiGranola);
+    lista.push(copiaAcaiGranola);
+    let mult = copiaAcaiGranola.qtd * copiaAcaiGranola.valor
+    precoAtualizado.push(Number(mult))
+    mostrarLista()
+}
+
+
+function poteDuo(){
+    poteDuo1eMeio.qtd = Number(quantidade.value);
+    if(poteDuo1eMeio.qtd === 0){
+        poteDuo1eMeio.qtd = 1;
+    }
+    const copiaPoteDuo1eMeio = Object.assign({}, poteDuo1eMeio);
+    lista.push(copiaPoteDuo1eMeio);
+    let mult = copiaPoteDuo1eMeio.qtd * copiaPoteDuo1eMeio.valor
+    precoAtualizado.push(Number(mult))
+    mostrarLista()
+}
+
+
+function pote2L(){
+    pote2l.qtd = Number(quantidade.value);
+    if(pote2l.qtd === 0){
+        pote2l.qtd = 1;
+    }
+    const copiaPote2l = Object.assign({}, pote2l);
+    lista.push(copiaPote2l);
+    let mult = copiaPote2l.qtd * copiaPote2l.valor
+    precoAtualizado.push(Number(mult))
+    mostrarLista()
+}
+
+
+function poteSpecialle(){
+    specialle.qtd = Number(quantidade.value);
+    if(specialle.qtd === 0){
+        specialle.qtd = 1;
+    }
+    const copiaSpecialle = Object.assign({}, specialle);
+    lista.push(copiaSpecialle);
+    let mult = copiaSpecialle.qtd * copiaSpecialle.valor
+    precoAtualizado.push(Number(mult))
+    mostrarLista()
+}
+
+
+function poteAcai2L(){
+    poteAcai2l.qtd = Number(quantidade.value);
+    if(poteAcai2l.qtd === 0){
+        poteAcai2l.qtd = 1;
+    }
+    const copiaPoteAcai2l = Object.assign({}, poteAcai2l);
+    lista.push(copiaPoteAcai2l);
+    let mult = copiaPoteAcai2l.qtd * copiaPoteAcai2l.valor
+    precoAtualizado.push(Number(mult))
+    mostrarLista()
+}
+
+
+function poteAcaiZero(){
+    acaiZero.qtd = Number(quantidade.value);
+    if(acaiZero.qtd === 0){
+        acaiZero.qtd = 1;
+    }
+    const copiaAcaiZero = Object.assign({}, acaiZero);
+    lista.push(copiaAcaiZero);
+    let mult = copiaAcaiZero.qtd * copiaAcaiZero.valor
     precoAtualizado.push(Number(mult))
     mostrarLista()
 }
