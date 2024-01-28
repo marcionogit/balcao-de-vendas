d0e4827912434ef5d85cf3b1050bbbb3bf1edd15
Márcio César <marcinho060498@gmail.com>
2024-01-28 10:07:59 -0300

    Adicionando cores / alterando estrutura da página / criando valor para o atacado

--- script.js
+++ script.js
@@ -41,147 +41,198 @@ const mostrarTroco = document.querySelector('#mostrarTroco');
 const leite = {
     descricao: 'AO LEITE',
     valor: 2.80,
+    valorAtacado: 2.50,
     qtd: 0
 }
 
 const fruta = {
     descricao: 'FRUTA',
     valor: 2.50,
+    valorAtacado: 2.25,
     qtd: 0
 }
 
 const trufa = {
     descricao: 'TRUFA GELADA',
     valor: 4.80,
+    valorAtacado: 4.50,
     qtd: 0
 }
 
 const retro = {
     descricao: 'RETRO',
     valor: 1.50,
+    valorAtacado: 1.50,
     qtd: 0
 }
 
 const astros = {
     descricao: 'ASTROS',
     valor: 4.50,
+    valorAtacado: 4.25,
     qtd: 0
 }
 
 const natturale = {
     descricao: 'NATTURALE',
     valor: 3.90,
+    valorAtacado: 3.60,
     qtd: 0
 }
 
 const acai = {
     descricao: 'AÇAI',
     valor: 4.75,
+    valorAtacado: 4.40,
     qtd: 0
 }
 
 const snack = {
     descricao: 'SNACK',
     valor: 5.90,
+    valorAtacado: 5.40,
     qtd: 0
 }
 
 const fondue = {
     descricao: 'FONDUE',
     valor: 3.80,
+    valorAtacado: 3.50,
     qtd: 0
 }
 
 const love = {
     descricao: 'LOVE',
     valor: 3.00,
+    valorAtacado: 3.00,
     qtd: 0
 }
 
 const unicornio = {
     descricao: 'UNICORNIO',
     valor: 3.90,
+    valorAtacado: 3.40,
     qtd: 0
 }
 
 const limaoSiciliano = {
     descricao: 'LIMÃO SICILIANO',
     valor: 4.90,
+    valorAtacado: 4.40,
     qtd: 0
 }
 
 const alpha = {
     descricao: 'ALPHA PREMIUM',
     valor: 5.90,
+    valorAtacado: 5.40,
     qtd: 0
 }
 
 const belgaBrulee = {
     descricao: 'BELGA / BRULEE',
     valor: 7.50,
+    valorAtacado: 6.90,
     qtd: 0
 }
 
 const paletas = {
     descricao: 'PALETAS',
     valor: 6.90,
+    valorAtacado: 6.60,
     qtd: 0
 }
 
 const brownieCenoura = {
     descricao: 'BROWNIE / CENOURA',
     valor: 8.50,
+    valorAtacado: 7.90,
     qtd: 0
 }
 
 const poteCopao = {
     descricao: 'COPÃO',
     valor: 5.75,
+    valorAtacado: 5.75,
     qtd: 0
 }
 
 const poteSundae = {
     descricao: 'SUNDAE',
     valor: 5.90,
+    valorAtacado: 5.60,
     qtd: 0
 }
 
 const acaiGranola = {
     descricao: 'AÇAI C/ GRANOLA',
     valor: 8.90,
+    valorAtacado: 8.60,
     qtd: 0
 }
 
 const poteDuo1eMeio = {
     descricao: 'POTE DUO 1,5L',
     valor: 15.90,
+    valorAtacado: 15.90,
     qtd: 0
 }
 
 const pote2l = {
     descricao: 'POTE 2L',
     valor: 19.90,
+    valorAtacado: 19.90,
     qtd: 0
 }
 
 const specialle = {
     descricao: 'POTE SPECIALLE',
     valor: 26.90,
+    valorAtacado: 26.90,
     qtd: 0
 }
 
 const poteAcai2l = {
     descricao: 'POTE AÇAI 2L',
     valor: 37.90,
+    valorAtacado: 35.90,
     qtd: 0
 }
 
 const acaiZero = {
     descricao: 'POTE AÇAI ZERO',
     valor: 37.90,
+    valorAtacado: 36.90,
     qtd: 0
 }
 
+const zeroPicole = {
+    descricao: 'PICOLÉ ZERO',
+    valor: 4.90,
+    valorAtacado: 4.40,
+    qtd: 0
+}
+
+const napolitanoZero = {
+    descricao: 'NAPOLITANO ZERO 1L',
+    valor: 26.90,
+    valorAtacado: 26.90,
+    qtd: 0
+}
+
+const bombom = {
+    descricao: 'MINI BOMBOM',
+    valor: 9.50,
+    valorAtacado: 8.90,
+    qtd: 0
+}
+
+const cone = {
+    descricao: 'MINI BOMBOM',
+    valor: 7.00,
+    valorAtacado: 9.50,
+    qtd: 0
+}
 
 
 
@@ -509,6 +560,58 @@ function poteAcaiZero(){
     const copiaAcaiZero = Object.assign({}, acaiZero);
     lista.push(copiaAcaiZero);
     let mult = copiaAcaiZero.qtd * copiaAcaiZero.valor
+    precoAtualizado.push(Number(mult))
+    mostrarLista()
+}
+
+
+function picoleZero(){
+    zeroPicole.qtd = Number(quantidade.value);
+    if(zeroPicole.qtd === 0){
+        zeroPicole.qtd = 1;
+    }
+    const copiaZeroPicole = Object.assign({}, zeroPicole);
+    lista.push(copiaZeroPicole);
+    let mult = copiaZeroPicole.qtd * copiaZeroPicole.valor
+    precoAtualizado.push(Number(mult))
+    mostrarLista()
+}
+
+
+function poteNapoZero(){
+    napolitanoZero.qtd = Number(quantidade.value);
+    if(napolitanoZero.qtd === 0){
+        napolitanoZero.qtd = 1;
+    }
+    const copiaNapolitanoZero = Object.assign({}, napolitanoZero);
+    lista.push(copiaNapolitanoZero);
+    let mult = copiaNapolitanoZero.qtd * copiaNapolitanoZero.valor
+    precoAtualizado.push(Number(mult))
+    mostrarLista()
+}
+
+
+function miniBombom(){
+    bombom.qtd = Number(quantidade.value);
+    if(bombom.qtd === 0){
+        bombom.qtd = 1;
+    }
+    const copiaBombom = Object.assign({}, bombom);
+    lista.push(copiaBombom);
+    let mult = copiaBombom.qtd * copiaBombom.valor
+    precoAtualizado.push(Number(mult))
+    mostrarLista()
+}
+
+
+function cones(){
+    cone.qtd = Number(quantidade.value);
+    if(cone.qtd === 0){
+        cone.qtd = 1;
+    }
+    const copiaCone = Object.assign({}, cone);
+    lista.push(copiaCone);
+    let mult = copiaCone.qtd * copiaCone.valor
     precoAtualizado.push(Number(mult))
     mostrarLista()
 }
